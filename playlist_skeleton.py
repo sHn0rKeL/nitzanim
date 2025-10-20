d041ecb0f51da132d00c38eae3d05ee508a027bc
benaya megadim <benayamfl2009@gmail.com>
2025-10-20 17:36:19 +0300

    Updated liked_songs dictionary data structure

---
+++ playlist_skeleton.py
@@ -0,0 +1,41 @@
+liked_songs = {
+    "Shake It Off": {
+        "artist": "Taylor Swift",
+        "duration": (3, 23),
+        "genre": "Pop"
+    },
+    "Shemesh": {
+        "artist": "Mergi",
+        "duration": (2, 33),
+        "genre": "Israeli"
+    },
+    "Chop Suey!": {
+        "artist": "System of a Down",
+        "duration": (3, 30),
+        "genre": "Metal"
+    },
+    "Mimaamakim": {
+        "artist": "Idan Raichel",
+        "duration": (4, 33),
+        "genre": "Israeli"
+    },
+    "Do I Wanna Know?": {
+        "artist": "Arctic Monkeys",
+        "duration": (4, 26),
+        "genre": "Rock"
+    },
+    "Love Story": {
+        "artist": "Taylor Swift",
+        "duration": (3, 55),
+        "genre": "Pop"
+    },
+    "Bo’ee": {
+        "artist": "Idan Raichel",
+        "duration": (4, 45),
+        "genre": "Israeli"
+    }
+}
+
+
+def main():
+    pass
